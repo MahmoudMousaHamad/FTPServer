248303463a311367337d04899c0d61731b7c4ad3
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2022-11-26 23:17:38 -0600

    init

--- receiver.c
+++ receiver.c
@@ -8,17 +8,23 @@
 #include <arpa/inet.h>
 #include <linux/tcp.h>
 #include <signal.h>
+#include <sys/stat.h>
+#include <time.h>
 
 #define BUFFER_SIZE 10000
 #define CLOSE "CLOSE"
+#define SEG_SIZE 256
 #define ERR "ERROR"
+#define NACK "NACK"
 #define ACK "ACK"
 
 static volatile int sockfd;
+static int packets_num, ack_num, nack_num;
 
 void intHandler() { close(sockfd); exit(0); }
 void exit_close(int socket) { close(socket); exit(5); }
-void send_ack(int socket) { puts("Sending ACK to sender..."); write(socket, ACK, sizeof(ACK));}
+void send_ack(int socket) { puts("Sending ACK to sender..."); write(socket, ACK, sizeof(ACK)); packets_num++;}
+void send_nack(int socket) { puts("Sending NACK to sender..."); write(socket, NACK, sizeof(NACK));packets_num++;}
 void get_from_user(char* p, char* name) {
 	printf("Please enter a %s: ", name);
 	fgets(p, BUFFER_SIZE, stdin);
@@ -28,11 +34,34 @@ int send_message(char* p, char* name, int socket, int size) {
 	if (size < 0) size = BUFFER_SIZE;
 	char buffer[size];
 	socklen_t addr_size;
-	if (sendto(socket, p, size, 0, (struct sockaddr*)NULL, addr_size) < 0) { perror("Send()"); exit_close(socket); }
+	if (sendto(socket, p, size, 0, (struct sockaddr*)NULL, addr_size) < 0) {perror("Send()"); exit_close(socket);packets_num++;}
 	printf("%s was sent to sender, waiting for ack...\n", name);
 	if (recv(socket, buffer, sizeof(buffer), 0) < 0) { perror("Receive()"); exit_close(socket); }
-	if (strcmp(buffer, ACK) == 0) return 1; 
+	if (strcmp(buffer, ACK) == 0) {printf("ACK received for %s\n", name); return 1;ack_num++;} 
 	return 0;
+}
+int receive_message(char **p, char *name, int sockfd) {
+	char buffer[BUFFER_SIZE];
+	printf("Waiting for %s...\n", name);
+	if (read(sockfd, buffer, BUFFER_SIZE) < 0) {perror("Read()"); exit(0);}
+	printf("Received %s: %s\n", name, buffer);
+	send_ack(sockfd);
+	strcpy(p, buffer);
+	return 1;
+}
+int receive_segment(int sockfd, char **segments) {
+	char buffer[BUFFER_SIZE];
+	puts("Waiting for segment...");
+	// Segment index
+	bzero(buffer, BUFFER_SIZE);
+	read(sockfd, buffer, BUFFER_SIZE);
+	int index = atoi(buffer);
+	// Segment data
+	read(sockfd, buffer, SEG_SIZE);
+	char *segment = malloc(BUFFER_SIZE);
+	strcpy(segment, buffer);
+	segments[index] = segment;
+	return index;
 }
 int getSend(char* p, char* name, int socket) {
 	char buffer[BUFFER_SIZE];
@@ -41,7 +70,16 @@ int getSend(char* p, char* name, int socket) {
 	printf("Sender did not acknowledge %s!\n", name); 
 	return 0;
 }
+const char* getFileCreationTime(char *path) {
+    struct stat attr;
+    stat(path, &attr);
+	char *buffer = malloc(BUFFER_SIZE);
+    sprintf(buffer, "%s", ctime(&attr.st_mtime));
+	return buffer;
+}
 int main(int argc, char ** argv) {
+	srand(time(NULL));
+
 	struct sockaddr_in server;
 	char buffer[BUFFER_SIZE];
 	socklen_t addr_size;
@@ -51,6 +89,7 @@ int main(int argc, char ** argv) {
 	char *sender_ip = argv[1];
 	int port = atoi(argv[2]);
 	char *input_filename = argv[3];
+	char *output_filename = argv[4];
 	// Sender address
 	server.sin_addr.s_addr = inet_addr(sender_ip);
     server.sin_port = htons(port);
@@ -62,9 +101,49 @@ int main(int argc, char ** argv) {
 	// Auth
 	send_message("AUTH", "AUTH", sockfd, sizeof("AUTH")); 
 	// while (1) { char username[BUFFER_SIZE], pwd[BUFFER_SIZE]; if (getSend(username, "Username", sockfd) && getSend(pwd, "Password", sockfd)) break; }
-	while (1) { if (send_message(input_filename, "Filename", sockfd, sizeof(input_filename))) break; }
+	// Filename
+	if (!send_message(input_filename, "File name", sockfd, sizeof(input_filename))) {puts("File was not found or is inaccessable."); exit(4);}
+	// Window size
+	char window_size[BUFFER_SIZE];
+	receive_message(&window_size, "Window Size", sockfd);
+	puts(window_size);
+	// Receive file
+	bzero(buffer, BUFFER_SIZE);
+	receive_message(&buffer, "Number of segments", sockfd);
+	int number_of_segments = atoi(buffer);
+	printf("Number of segments: %d\n", number_of_segments);
+	char *segments[number_of_segments];
+	int segment_index = 0;
+	int window_start, window_end;
+	for(int i = 0; i < number_of_segments; i++) {
+		int index = receive_segment(sockfd, segments);
+		if (rand() % 100 + 0 < 0) {send_ack(sockfd);}
+		else {send_nack(sockfd); receive_segment(sockfd, segments);}
+	}
+	// Write segments to file
+	FILE *fp = fopen(output_filename, "w+");
+	for (int i = 0; i < number_of_segments; i++) {
+		fprintf(fp, "%s", segments[i]);
+	}
+	FILE* f = fopen(output_filename, "rb");
+	fseek(f, 0L, SEEK_END);
+	long f_len = ftell(f);
+	fseek(f, 0L, SEEK_SET);
+	printf(
+			"Name and Size of file received: %s, %ld bytes\n"
+			"File Creation Date & Time: %s\n"
+			"Number of Data Packets Transmitted: %d\n"
+			"Number of Acknowledgement Received: %d\n"
+			"Number of Negative Acknowledgement Received with Sequence Number: %d\n",
+			input_filename, f_len,
+			getFileCreationTime(output_filename),
+			packets_num,
+			ack_num,
+			nack_num
+		);
     puts("Exiting...");
 	close(sockfd);
+	fclose(fp);
     exit(0);
 	return 0;
 }
--- sender.c
+++ sender.c
@@ -7,21 +7,24 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <linux/tcp.h>
+#include <sys/stat.h>
 #include <signal.h>
 #include <math.h>
+#include <time.h>
 
+#define FILE_END "FILE_END"
 #define BUFFER_SIZE 10000
 #define SEG_SIZE 256
 #define CLOSE "CLOSE"
 #define ERR "ERROR"
-#define ACK "ACK"
 #define NACK "NACK"
+#define ACK "ACK"
+
 static volatile int sockfd, receiver;
+static int packets_num, packets_num_retransmitted, ack_num, nack_num;
 
 void intHandler() { close(sockfd); }
-
 void exit_close(int socket) { close(socket); exit(5); }
-
 int findInArray(char* thing, char* list[], int size, int index) {
 	if (index >= 0) {
 		if(strcmp(thing, list[index]) == 0) return index; 
@@ -31,66 +34,35 @@ int findInArray(char* thing, char* list[], int size, int index) {
 	for (int i = 0; i < size; i++) if (strcmp(thing, list[i]) == 0) return i;
 	return -1;
 }
-
 int send_message(char* p, char* name, int socket, int size) {
 	if (size < 0) size = BUFFER_SIZE;
 	char buffer[size];
-	if (send(socket, p, size, 0) < 0) { perror("Send()"); exit_close(socket); }
-	printf("%s was sent to receiver\n", name);
+	socklen_t addr_size;
+	if (write(socket, p, size) < 0) { perror("Write()"); exit_close(socket); }
+	packets_num++;
+	printf("%s with value %s was sent to receiver, waiting for ack...\n", name, p);
 	if (recv(socket, buffer, sizeof(buffer), 0) < 0) { perror("Receive()"); exit_close(socket); }
-	if (strcmp(buffer, ACK) == 0) return 1; 
+	if (strcmp(buffer, ACK) == 0){printf("ACK received for %s\n", name); return 1;ack_num++;} 
 	return 0;
 }
-
-int receive_int(int *num, int fd) {
-    int32_t ret;
-    char *data = (char*)&ret;
-    int left = sizeof(ret);
-    int rc;
-    do {
-        rc = read(fd, data, left);
-        if (rc >= 0) {
-			data += rc;
-            left -= rc;
-		}
-    } while (left > 0);
-    *num = ntohl(ret);
-    return 0;
-}
-
-int send_int(int num, int fd) {
-    int32_t conv = htonl(num);
-    char *data = (char*)&conv;
-    int left = sizeof(conv);
-    int rc;
-    do {
-        rc = write(fd, data, left);
-        if (rc >= 0) {
-			data += rc;
-            left -= rc;
-		}
-    } while (left > 0);
-    return 0;
-}
-
-int send_int_to(int integer, char* name, int socket) {
+int receive_message(char **p, char *name, int sockfd) {
 	char buffer[BUFFER_SIZE];
-	if (send_int(integer, socket) < 0) { perror("Send()"); exit_close(socket); }
-	printf("%s was sent to receiver\n", name);
+	printf("Waiting for %s...\n", name);
+	if (read(sockfd, buffer, BUFFER_SIZE) < 0) {perror("Read()"); exit(0);}
+	printf("Received %s: %s\n", name, buffer);
+	send_ack(sockfd);
+	strcpy(p, buffer);
+	return 1;
+}
+void send_ack(int socket) {puts("Sending ACK to sender...");write(socket, ACK, sizeof(ACK));packets_num++;}
+int receive_ack(int socket, char *name) {
+	char buffer[BUFFER_SIZE];
+	printf("Waiting for ack for %s...\n", name);
 	if (recv(socket, buffer, sizeof(buffer), 0) < 0) { perror("Receive()"); exit_close(socket); }
-	if (strcmp(buffer, ACK) == 0) {puts("Receiver ACK int"); return 1;}
+	if (strcmp(buffer, ACK) == 0) {printf("ACK received for %s\n", name); return 1;ack_num++;} 
 	return 0;
 }
-
-void send_ack(int socket) {
-	puts("Sending ACK to sender...");
-	write(socket, ACK, sizeof(ACK));
-}
-void send_nack(int socket) {
-	puts("Sending NACK to sender...");
-	write(socket, NACK, sizeof(NACK));
-}
-
+void send_nack(int socket) {puts("Sending NACK to sender...");write(socket, NACK, sizeof(NACK));packets_num++;}
 int read_ack_verify(char* p, char* name, char* list[], int size, int socket, int index) {
 	char buff[BUFFER_SIZE];
 	printf("Waiting to receive %s from sender...\n", name);
@@ -108,7 +80,24 @@ int read_ack_verify(char* p, char* name, char* list[], int size, int socket, int
 	send(socket, ERR, sizeof(ERR), 0);
 	return i;
 }
-
+void send_segment(int sockfd, char **segments, int index) {
+	char buffer[BUFFER_SIZE];
+	sprintf(buffer, "%d", index);
+	// Segment index
+	write(sockfd, buffer, BUFFER_SIZE);
+	packets_num++;
+	// Segment data
+	printf("Sending segment #%d\n", index);
+	write(sockfd, segments[index], SEG_SIZE);
+	packets_num++;
+}
+const char* getFileCreationTime(char *path) {
+    struct stat attr;
+    stat(path, &attr);
+	char *buffer = malloc(BUFFER_SIZE);
+    sprintf(buffer, "%s", ctime(&attr.st_mtime));
+	return buffer;
+}
 int main(int argc, char ** argv) {
 	char username[BUFFER_SIZE], pwd[BUFFER_SIZE], buffer[BUFFER_SIZE];
 	int username_index = -1, pwd_index = -1, client_len;
@@ -119,7 +108,7 @@ int main(int argc, char ** argv) {
 	// Args check
     if (argc != 3) { fprintf(stderr, "Usage: %s port window_size\n", argv[0]); exit(1); }
 	int port = atoi(argv[1]);
-	int window_size = atoi(argv[2]);
+	char *window_size = argv[2];
 	// Stream socket
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0){perror("Socket()");exit(2);}
 	puts("Server Socket created successfully");
@@ -137,48 +126,72 @@ int main(int argc, char ** argv) {
 	char* passwords[] = {"a86H6T0c","G6M7p8az","Pd82bG57","jO79bNs1","Cfw61RqV","Kuz07YLv"};
 	// Main loop
 	while (1) {
+		puts("Waiting for datagrams...");
 		if (recvfrom(sockfd, (char *)buffer, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr*)&client, &client_len) < 0) { perror("recvfrom()"); exit(1);}
 		printf("Received message %s from %s:%d\n", buffer, inet_ntoa(client.sin_addr), ntohs(client.sin_port));
 		if(connect(sockfd, (struct sockaddr *)&client, client_len) < 0) { perror("Connect()"); exit(0); }
-		// send_ack(sockfd);
+		send_ack(sockfd);
 		// // Auth
 		// while (1) {
 		// 	if ((username_index = read_ack_verify(username, "Username", usernames, 6, sockfd, -1)) < 0) continue; 
 		// 	if ((pwd_index = read_ack_verify(pwd, "Password", passwords, 6, sockfd, username_index)) >= 0) break; 
 		// }
 		// File name
-		while (1) {
-			puts("Waiting for filename...");
-			if (read(sockfd, filename, BUFFER_SIZE) < 0) {perror("Read()"); exit(0);}
-			printf("Received filename: %s\n", filename);
-			send_ack(sockfd);
-			if (access(filename, F_OK) == 0) {puts("File found and is accessible"); send_ack(sockfd); break;}
-			else {puts("File not accessible"); send_nack(sockfd);};
-		}
+		puts("Waiting for filename...");
+		if (read(sockfd, filename, BUFFER_SIZE) < 0) {perror("Read()"); exit(0);}
+		printf("Received filename: %s\n", filename);
+		if (access(filename, F_OK) == 0) {puts("File found and is accessible"); send_ack(sockfd);}
+		else {puts("File not accessible"); send_nack(sockfd); break;};
 		// Send window size
-		puts("Sending window size");
-		send_int_to(window_size, "Window Size", receiver);
+		send_message(window_size, "Window Size", sockfd, sizeof(window_size));
 		// Transmit file
 		FILE* f = fopen(filename, "rb");
+		fseek(f, 0L, SEEK_END);
 		long f_len = ftell(f);
-		int number_of_segments = ceil(f_len / SEG_SIZE);
+		fseek(f, 0L, SEEK_SET);
+		printf("File len:%d\n", f_len);
+		int number_of_segments = ceil(f_len / SEG_SIZE) + 1;
+		// Send number of segments
+		bzero(buffer, BUFFER_SIZE);
+		sprintf(buffer, "%d", number_of_segments);
+		send_message(buffer, "Number of segments", sockfd, BUFFER_SIZE);
 		char* segments[number_of_segments];
 		puts("Preparing segments...");
 		for (int cur_seg = 0; cur_seg < number_of_segments; cur_seg++) {
 			char* segment_buffer = (char*) malloc(SEG_SIZE * sizeof(char));
 			fread(segment_buffer, SEG_SIZE, 1, f);
-			strcpy(segments[cur_seg], segment_buffer);
+			segments[cur_seg] = segment_buffer;
 		}
 		puts("Sending segments...");
 		int window_start = 0;
-		int window_end = window_start + window_size;
-		while(1) {
-			for (int i = window_start; i < window_end; i++) {
-				// send_segment(segments[i], i, sockfd);
-				write(sockfd, segments[i], SEG_SIZE);
-			}
+		int window_end = atoi(window_size) > number_of_segments ? number_of_segments : window_start + atoi(window_size);
+		int current_frame;
+		for (current_frame = window_start; current_frame < number_of_segments; current_frame++) {
+			send_segment(sockfd, segments, current_frame);
+			bzero(buffer, BUFFER_SIZE);
+			read(sockfd, buffer, BUFFER_SIZE);
+			if (strcmp(ACK, buffer) == 0) {ack_num++; continue;}
+			else if (strcmp(NACK, buffer) == 0) {nack_num++; packets_num_retransmitted++; send_segment(sockfd, segments, current_frame);}
 		}
+		printf(
+			"Receiver IP and Port Number: %s:%d\n"
+			"Name and Size of file: %s, %ld bytes\n"
+			"File Creation Date & Time: %s\n"
+			"Number of Data Packets Transmitted: %d\n"
+			"Number of Packets Re-transmitted: %d\n"
+			"Number of Acknowledgement Received: %d\n"
+			"Number of Negative Acknowledgement Received with Sequence Number: %d\n",
+			inet_ntoa(client.sin_addr), ntohs(client.sin_port),
+			filename, f_len,
+			getFileCreationTime(filename),
+			packets_num,
+			packets_num_retransmitted,
+			ack_num,
+			nack_num
+		);
+		break;
 	}
+	puts("Exiting...");
 	close(sockfd);
 	exit(0);
 	return 0;
